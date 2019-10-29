67c771e8bddafeef06d106d04b9900226cab1e7c
bruce.gao <yongkang1987@126.com>
2019-10-29 17:57:27 +0800

    修改获取路径公共方法

--- common/getpathInfo.py
+++ common/getpathInfo.py
@@ -1,8 +1,8 @@
 import os
 
 def get_Path():
-    path = os.path.split(os.path.realpath(__file__))[0]
-    return path[:20]
+    path = os.path.split(os.path.realpath(__file__))[0][:-7]
+    return path
 
 if __name__ == '__main__':# 执行该文件，测试下是否OK
     print('测试路径是否OK,路径为：', get_Path())
---
+++ testCase/TestCase_TaskType.py
@@ -0,0 +1,4 @@
+#coding=utf-8
+#author_="bruce.gao"
+#date:2019/10/29 16:17
+
